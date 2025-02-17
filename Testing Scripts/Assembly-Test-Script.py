cff01312d3c44fa3e07d6eaaab497b1a5d68943d
nschweikASU <nschweik@asu.edu>
2025-02-17 14:34:23 -0800

    Noah-2-17-Creating Folders

---
+++ Testing Scripts/Assembly-Test-Script.py
@@ -0,0 +1,5 @@
+# TODO
+#
+# 1) Read in assembly code
+# 2) Read in test cases
+# 3) Run test Cases
---
+++ Testing Scripts/C-Test-Script.py
@@ -0,0 +1,6 @@
+# TODO
+#
+# 1) Read in C code
+# 2) Read in test cases
+# 3) Compile C code
+# 4) Run test Cases
